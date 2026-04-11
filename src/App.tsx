6ba8ec47d3015fcfcaee28e0f78216005dde774a
Viper <pruncle21@gmail.com>
2026-04-11 16:19:34 -0500

    Organized the tailwind css utility classes according to CUBE principles.

--- src/App.tsx
+++ src/App.tsx
@@ -9,39 +9,39 @@ import imgAvatar from "./assets/images/image-avatar.png";
 
 export default function App() {
   return (
-    <div className="parent-container min-h-screen p-5 flex flex-col justify-center items-center bg-slate-900 text-slate-500"> 
+    <div className="[ parent__container ] [ min-h-screen ] [ p-5 ] [ flex flex-col justify-center items-center ] [ bg-slate-900 ] [ text-slate-500 ]"> 
       <main className="bg-slate-800 rounded-lg grid grid-cols-1 auto-rows-min gap-5 p-5 w-[335px] h-[576px] ring-20 ring-gray-900 shadow-2xl/100">
         <section>
-          <div className="img-container">
+          <div className="img__container">
             <img src={nftImage} className="rounded-md" aria-hidden="true" />
           </div>
         </section>
         <section className="grid gap-5">
-          <h1 className="text-xl text-white font-bold hover:text-cyan-300"><a href="#">Equilibrium #3429</a></h1>
+          <h1 className="[ text-xl text-white hover:text-cyan-300 ] [ font-bold ]"><a href="#">Equilibrium #3429</a></h1>
           <p>Our Equilibrium collection promotes balance and calm.</p>
         </section>
         <section className="flex justify-between">
-          <div className="flex gap-2 items-center">
+          <div className="flex items-center gap-2">
             <img src={iconEthereum} className="h-4" aria-hidden="true" />
-            <p className="text-cyan-300 font-medium">0.041 ETH</p>
+            <p className="[ text-cyan-300 ] [ font-medium ]">0.041 ETH</p>
           </div>
-          <div className="flex gap-2 items-center">
+          <div className="flex items-center gap-2">
             <img src={iconClock} className="h-4" aria-hidden="true" />
             <p>3 days left</p>
           </div>
         </section>
         <hr />
         <section>
-          <div className="flex gap-4 items-center">
-            <img src={imgAvatar} className="h-10 border-2 border-slate-300 rounded-[50%]" aria-hidden="true" />
+          <div className="flex items-center gap-4">
+            <img src={imgAvatar} className="[ h-10 ] [ border-2 border-slate-300 rounded-[50%] ]" aria-hidden="true" />
             <p>Creation of <a href="#" className="text-white hover:text-cyan-300">Jules Wyvern</a></p>
           </div>
         </section>
       </main>
 
-      <footer className="attribution fixed bottom-0 m-5 text-center text-white font-light">
-        <p>Challenge by <a href="https://www.frontendmentor.io?ref=challenge"><span className="text-blue-500 font-medium">Frontend Mentor</span></a></p> 
-        <p>Coded by <a href="#"><span className="text-blue-500 font-medium">Joshua Martinez</span></a></p>
+      <footer className="[ attribution ] [ fixed bottom-0 ] [ m-5 ] [ text-center text-white ] [ font-light ]">
+        <p>Challenge by <a href="https://www.frontendmentor.io?ref=challenge"><span className="[ text-blue-500 ] [ font-medium ]">Frontend Mentor</span></a></p> 
+        <p>Coded by <a href="#"><span className="[ text-blue-500 ] [ font-medium ]">Joshua Martinez</span></a></p>
       </footer>
     </div>
   )
