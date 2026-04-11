270c6bd73842cc6cd36bff68954ca974d098e176
Viper <pruncle21@gmail.com>
2026-04-11 14:25:47 -0500

    Resolved errors flagged by typescript.

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export default function App() {
       
       Creation of Jules Wyvern
 
-      <footer class="attribution">
+      <footer className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
         Coded by <a href="#">Your Name Here</a>.
       </footer>
