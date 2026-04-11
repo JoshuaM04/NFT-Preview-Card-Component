e085e71d749e7661e0eb69f022f3a4fdf283def8
Viper <pruncle21@gmail.com>
2026-04-11 14:40:29 -0500

    Added the structure for the webpage using semantic elements.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
+// @ts-ignore
+import nftImage from "./assets/images/image-equilibrium.jpg";
+
 export default function App() {
   return (
-    <div> 
-      Equilibrium #3429
-
-      Our Equilibrium collection promotes balance and calm.
-
-      0.041 ETH
-      3 days left
+    <div className="parent-container min-h-screen p-5 flex flex-col justify-center items-center"> 
+      <main>
+        <section>
+          <div className="img-container">
+            <img src={nftImage} aria-hidden="true" />
+          </div>
+        </section>
+        <section>
+          Equilibrium #3429
+          Our Equilibrium collection promotes balance and calm.
+        </section>
+        <section>
+          0.041 ETH
+          3 days left
+        </section>
+        <section>
+          Creation of Jules Wyvern
+        </section>
+      </main>
       
-      Creation of Jules Wyvern
-
       <footer className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
         Coded by <a href="#">Your Name Here</a>.
