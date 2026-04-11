87ab98345b97cf0a32fb1a15bf58dd730c3053ca
Viper <pruncle21@gmail.com>
2026-04-11 12:05:21 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+export default function App() {
+  return (
+    <div> 
+      Equilibrium #3429
+
+      Our Equilibrium collection promotes balance and calm.
+
+      0.041 ETH
+      3 days left
+      
+      Creation of Jules Wyvern
+
+      <footer class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+        Coded by <a href="#">Your Name Here</a>.
+      </footer>
+    </div>
+  )
+}
